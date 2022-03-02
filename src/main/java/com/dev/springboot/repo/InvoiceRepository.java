cd48dd1f307eb2122b64d9ec4c784af746f1e043
whpark70 <whpark70@gmail.com>
2022-03-02 17:27:23 +0900

    layout #2

--- src/main/java/com/dev/springboot/repo/InvoiceRepository.java
+++ src/main/java/com/dev/springboot/repo/InvoiceRepository.java
@@ -3,5 +3,5 @@ package com.dev.springboot.repo;
 import com.dev.springboot.model.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class InvoiceRepository extends JpaRepository<Invoice, Long> {
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
 }
